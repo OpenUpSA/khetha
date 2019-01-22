f00edd03108a7cae13f845fca799bd680ed2b549
Schalk Venter <venterschalk@gmail.com>
2019-01-22 23:29:36 +0200

    Removes SSR processing of GraphQL data

--- src/tooling/gatsby-node/getFolderTranslations/getFirstIntanceInArray.js
+++
@@ -1,9 +0,0 @@
-const { isoToLanguage } = require('../../../helpers/languageConversions/index.node');
-
-
-const findLanguageInArray = isoKey => ({ language }) => language === isoToLanguage(isoKey);
-
-module.exports = ({ translations, isoKey }) => {
-  const resultArray = translations.filter(findLanguageInArray(isoKey));
-  return resultArray[0] || false;
-};
