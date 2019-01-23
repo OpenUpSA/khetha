51b5108292304ff802581f7b8962b8b2a9dfacd1
AliciadW <alicia.dewet@living-stratus.com>
2019-01-23 06:27:34 +0200

    Created SubHeading component

--- src/views/LoginPage.jsx
+++ src/views/LoginPage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 
 import MainHeading from '../components/MainHeading';
+import SubHeading from '../components/SubHeading';
 
 const StyledWrapper = styled.div`
   font-family: "Roboto", sans-serif;
@@ -22,27 +23,15 @@ const StyledWrapper = styled.div`
   flex-direction: column;
 `;
 
-const SubHeading = styled.h3`
-  position: relative;
-  text-align: center;
-  font-size: 17px;
-  font-weight: normal;
-  line-height: 22px;
-  letter-spacing: -0.41px;
-  color: #8C8C8C;
-  max-width: 295px;
-  margin: 0 0 30px 0;
-`;
-
 const LandingPage = () => (
   <React.Fragment>
     <StyledWrapper>
       <MainHeading text="Log In" />
-      <SubHeading>
-        In order to save your progress you need to
+      <SubHeading
+        text="In order to save your progress you need to
         sign in to Kheta with one of the following
-        services:
-      </SubHeading>
+        services:"
+      />
     </StyledWrapper>
   </React.Fragment>
 );
