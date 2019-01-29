cad77674562a43b8c46db6176a9c47801bfbe645
Schalk Venter <venterschalk@gmail.com>
2019-01-29 12:43:01 +0200

    Fixes img error

--- src/components/Layout/MenuHeader.jsx
+++ src/components/Layout/MenuHeader.jsx
@@ -1,39 +1,35 @@
 import React from 'react';
-import MainHeading from '../MainHeading';
-import logo from '../../images/logo.svg';
 import styled from 'styled-components';
+import MainHeading from '../MainHeading';
+// import logo from '../../images/logo.svg';
+
 
 const HeaderMenu = styled.div`
   display: flex;
   width: 100%;
-`
+`;
+
 const Brand = styled.img`
   padding: 24px 16px;
   height: 58.79px;
   right: 80.23%;
-`
+`;
 
 const Text = styled.div`
   flex-grow: 1
 `;
 
 
-const MenuHeader = props => {
+const MenuHeader = () => (
+  <HeaderMenu>
+    <Brand>
+      {/* <img width={100} height={100} src={logo} alt="" /> */}
+    </Brand>
+    <Text>
+      <MainHeading title="Honey Bee Level" />
+    </Text>
+  </HeaderMenu>
+);
 
-  return (
-    <HeaderMenu>
-      <Brand>
-        <img width={100} height={100} src={logo} />
-      </Brand>
-      <Text>
-        <MainHeading title="Honey Bee Level" />
-      </Text>
-    </HeaderMenu>
-  );
-}
 
 export default MenuHeader;
-
-
-
-
