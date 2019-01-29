ebd1840b1a0ac2d494bf5baf6cc1e37371a2e2c2
Schalk Venter <venterschalk@gmail.com>
2019-01-29 08:39:47 +0200

    Updates connection between onboarding view and redux store

--- src/views/Onboarding/index.js
+++ src/views/Onboarding/index.js
@@ -1,14 +1,25 @@
 import { connect } from 'react-redux';
 import State from './State';
+import { setUserValue } from '../../redux/modules/user';
 
 
-const mapStateToProps = ({ user }, ownProps) => ({
-  language: user.ownProps,
+const mapStateToProps = ({ user }, ownProps) => {
+  return {
+    language: user.language.value,
+    onboardingLevel: user.onboarding.value,
+    ...ownProps,
+  };
+};
+
+
+const mapDispatchToProps = (dispatch, ownProps) => ({
+  changeLanguage: isoKey => dispatch(setUserValue({ language: isoKey })),
+  changeOnboarding: percent => dispatch(setUserValue({ onboarding: percent })),
   ...ownProps,
 });
 
 
-const connectToStore = connect(mapStateToProps);
+const connectToStore = connect(mapStateToProps, mapDispatchToProps);
 
 
 export default connectToStore(State);
