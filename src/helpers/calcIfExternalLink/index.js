bc1f4e2d1bd3eb52b2ba126bb9cdb7b685d6c1c0
Schalk Venter <venterschalk@gmail.com>
2019-01-29 22:43:27 +0200

    Fixes linting issues

--- src/helpers/calcIfExternalLink/index.js
+++ src/helpers/calcIfExternalLink/index.js
@@ -2,11 +2,11 @@ import propsCheck, { PropTypes } from '../propsCheck';
 
 
 const calcIfExternalLink = (props) => {
-  propsCheck({ check: calcIfExternalLink, props })
+  propsCheck({ check: calcIfExternalLink, props });
   const { url } = props;
   const condition = new RegExp(`/${window.location.host}/`);
   return condition.test(url);
-}
+};
 
 
 calcIfExternalLink.propTypes = {
