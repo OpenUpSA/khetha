4b296c7d60a3a23a0bf4a52d2948b37e882764f8
Schalk Venter <venterschalk@gmail.com>
2019-01-29 07:36:00 +0200

    Disables routing overrides for debugging purposes

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,12 +2,15 @@
 
 import { createElement } from 'react';
 import { Provider } from 'react-redux';
-import { navigate } from 'gatsby';
-
-import { version as targetStorageVersion } from './src/config/storage.json';
-import { setVersion as setStorageVersion } from './src/redux/modules/storage';
-import { initStateFromRemote, syncWithRemote } from './src/redux/actions';
+// import { navigate } from 'gatsby';
+// import { version as targetStorageVersion } from './src/config/storage.json';
+// import { setVersion as setStorageVersion } from './src/redux/modules/storage';
 import store from './src/redux/store';
+import {
+  initStateFromRemote,
+  syncWithRemote,
+  checkOfflineQueue,
+} from './src/redux/actions';
 
 
 export const wrapRootElement = ({ element }) => (
@@ -15,32 +18,32 @@ export const wrapRootElement = ({ element }) => (
 );
 
 
-const wipeStorage = ({ version }) => {
-  window.localStorage.clear();
-  store.dispatch(setStorageVersion({ version }));
-};
-
-
-export const onInitialClientRender = () => {
-  const { user } = store.getState();
-  const { language } = user;
-
-  if (!language) {
-    return navigate('/en/');
-  }
-
-  return navigate(`/${language}/start`);
-};
-
-
 export const onClientEntry = () => {
-  const { storage } = store.getState();
-  const { version: storageVersion } = storage;
+  // const { user, storage } = store.getState();
 
-  if (storageVersion < targetStorageVersion) {
-    wipeStorage({ version: targetStorageVersion });
-  }
+  // const { version: storageVersion } = storage;
+  // const { value: language } = user.language;
+  // const { value: onboarding } = user.onboarding;
 
-  store.dispatch(initStateFromRemote())
+  // if (storageVersion < targetStorageVersion) {
+  //   wipeStorage({ version: targetStorageVersion });
+  // }
+
+  // if (!language) {
+  //   return navigate('/en/');
+  // }
+
+  // if (onboarding <= 2) {
+  //   return navigate(`/${language}/`);
+  // }
+
+  // return navigate(`/${language}/start/`);
+
+  const intervalSync = () => store
+    .dispatch(checkOfflineQueue())
+    .then(() => store.dispatch(initStateFromRemote()))
     .then(() => store.dispatch(syncWithRemote()));
+
+  intervalSync();
+  setInterval(intervalSync, 60000);
 };
