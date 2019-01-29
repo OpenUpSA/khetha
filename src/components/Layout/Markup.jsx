12b946cc2086bf247f262ad642ba78ef52172da1
Schalk Venter <venterschalk@gmail.com>
2019-01-29 13:04:48 +0200

    Fallback value for MenuHeader points

--- src/components/Layout/Markup.jsx
+++ src/components/Layout/Markup.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import MenuHeader from './MenuHeader';
 
 
-export default ({ children, points }) => (
+export default ({ children, points = 0 }) => (
   <div>
     <MenuHeader {...{ points }} />
     {children}
