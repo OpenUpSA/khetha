cee9197460656a2c89fe2a97aada2a27977d775a
Mish <mishuct26@gmail.com>
2019-01-30 10:35:43 +0200

    Remove "git" typo-error

--- src/components/LevelBadge/index.jsx
+++ src/components/LevelBadge/index.jsx
@@ -1,30 +1,31 @@
 import React from 'react';
 import styled from 'styled-components';
-import schema from './schema';
+import PropTypes from 'prop-types';
 import SvgIcon from './SvgIcon';
 import ShieldIcon from './ShieldIcon';
-
 
 
 const PositionWrapper = styled.div`
   position: relative;
 `;
 
-const LevelBadge = ({ level = 0 }) => git (
+
+const LevelBadge = ({ level = 0 }) => (
   <PositionWrapper>
     <SvgIcon {...{ level }} />
     <ShieldIcon {...{ level }} />
   </PositionWrapper>
 );
 
-
 export default LevelBadge;
-
 
 LevelBadge.propTypes = {
   /** User level number to determine what icon
    * and number in the shield gets returned  */
-  level: schema.propTypes.level,
+  level: PropTypes.number,
 };
 
-LevelBadge.defaultProps = schema.defaultProps;
+
+LevelBadge.defaultProps = {
+  level: 0,
+};
