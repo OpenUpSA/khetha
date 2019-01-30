3e96766e0895fbfa2224f2d923f951ee6167589c
Schalk Venter <venterschalk@gmail.com>
2019-01-30 12:07:21 +0200

    Uncomments Start view

--- src/views/Start.jsx
+++ src/views/Start.jsx
@@ -1,9 +1,9 @@
-// import React from 'react';
-// import Layout from '../components/Layout';
+import React from 'react';
+import Layout from '../components/Layout';
 
 
-// export default props => (
-//   <Layout>
-//     Hello
-//   </Layout>
-// );
+export default props => (
+  <Layout>
+    Hello
+  </Layout>
+);
