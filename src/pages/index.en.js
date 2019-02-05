c80e29c603850e6748ff5cde9de86437d6661ea4
Schalk Venter <venterschalk@gmail.com>
2019-02-05 22:38:33 +0200

    Stub pages while views are created

--- src/pages/index.en.js
+++ src/pages/index.en.js
@@ -1,5 +1,19 @@
 import { createElement } from 'react';
-import OnBoarding from '../views/Onboarding';
+
+export default () => createElement('div', {}, 'Placeholder');
+
+// // import { connect } from 'react-redux';
+// import { createElement } from 'react';
+// // import OnBoarding from '../views/Onboarding';
 
 
-export default () => createElement(OnBoarding);
+// // const mapStateToProps = ({ info }, ownProps) => ({
+// //   language: info.ownProps,
+// //   ...ownProps,
+// // });
+
+// // const connectToStore = connect(mapStateToProps);
+// // const view = connectToStore('OnBoarding');
+
+
+// export default () => createElement('div');
--- src/pages/index.zu.js
+++
@@ -1,5 +0,0 @@
-import { createElement } from 'react';
-import OnBoarding from '../views/Onboarding';
-
-
-export default () => createElement(OnBoarding);
