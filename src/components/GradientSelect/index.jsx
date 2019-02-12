de0553b1b622676989484d1b6f97fb7b8c4ea5da
Schalk Venter <venterschalk@gmail.com>
2019-02-12 09:21:39 +0200

    Update GradientSelect component

--- src/components/GradientSelect/index.jsx
+++ src/components/GradientSelect/index.jsx
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import t from 'prop-types';
 import Dropdown from './Dropdown';
 
 
 class GradientSelect extends Component {
-  state = {
-    selected: this.props.value || null,
+  constructor(props) {
+    super(props);
+    const { value } = this.props;
+
+    this.state = {
+      selected: value || null,
+    };
   }
 
   changeSelected = (value) => {
@@ -45,27 +50,29 @@ export default GradientSelect;
 GradientSelect.propTypes = {
   /** An array of objects that describe an option
    * and what should happen when it is clicked */
-  options: PropTypes.arrayOf(
-    PropTypes.shape({
-      text: PropTypes.string,
-      callback: PropTypes.func,
-      reset: PropTypes.bool,
+  options: t.arrayOf(
+    t.shape({
+      text: t.string,
+      callback: t.func,
+      reset: t.bool,
     }),
   ).isRequired,
+  /** Forces the dropdown to have a certain value selected when it renders  */
+  value: t.string,
   /** The text to show when no option has been selected */
-  placeholder: PropTypes.string,
+  placeholder: t.string,
   /** Whether to replace the normal background colour
    * with the brand gradient */
-  filled: PropTypes.bool,
+  filled: t.bool,
   /** Whether the button should span the entirity (100%)
    * of the width of it's parent. */
-  full: PropTypes.bool,
+  full: t.bool,
   /** Calls this function with the value of selection
    * option as first parameter */
-  callback: PropTypes.func,
-  /* String applied before (styled as lable) value
+  callback: t.func,
+  /** String applied before (styled as lable) value
   in button when a value is selected */
-  prefix: PropTypes.string,
+  prefix: t.string,
 };
 
 
@@ -75,4 +82,5 @@ GradientSelect.defaultProps = {
   callback: null,
   full: false,
   prefix: null,
+  value: null,
 };
