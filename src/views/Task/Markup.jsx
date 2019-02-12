582c95236a059c593437f878f50996729c1f8390
Schalk Venter <venterschalk@gmail.com>
2019-02-12 09:18:23 +0200

    Updates Task view

--- src/views/Task/Markup.jsx
+++ src/views/Task/Markup.jsx
@@ -1,62 +1,16 @@
-import React, { Fragment, Component } from 'react';
-import TextField from '@material-ui/core/TextField';
+import React, { Fragment } from 'react';
 import styled from 'styled-components';
+import t from 'prop-types';
 import { Typography } from '@material-ui/core';
-import FormGroup from '@material-ui/core/FormGroup';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
 import SectionHeading from '../../components/SectionHeading';
 import ProgressList from '../../components/ProgressList';
-import GradientButton from '../../components/GradientButton';
-import GradientSelect from '../../components/GradientSelect';
+import CreateInput from './CreateInput';
 
 
 const Section = styled.div`
   padding-bottom: 40px;
 `;
 
-
-class FormBinder extends Component {
-  constructor(props) {
-    super(props);
-    const { forceValue, array } = this.props;
-    const realValue = array ? forceValue.split(', ') : forceValue;
-
-    this.state = {
-      value: realValue || null,
-    };
-  }
-
-  updateArray = (index, newValue, array) => {
-    const temp = array;
-    temp[index] = newValue;
-
-    this.setState({
-      value: temp,
-    });
-  }
-
-  updateValue = value => this.setState({ value });
-
-  render() {
-    const { props, state, ...events } = this;
-
-    return (
-      <Fragment>
-        {props.callback(state.value, events.updateValue, events.updateArray)}
-      </Fragment>
-    );
-  }
-}
-
-
-const FlexWrap = styled.div`
-  display: flex;
-`;
-
-const ButtonWrap = styled.div`
-  padding-right: 30px;
-`;
 
 const BodyText = styled(Typography)`
   && {
@@ -65,181 +19,8 @@ const BodyText = styled(Typography)`
   }
 `;
 
-const SubmitWrap = styled.div`
-  padding-top: 20px;
-`;
 
-
-const calcInput = (type, value, options, focus, next, changeAnswer, index) => {
-  if (type === 'checkboxes') {
-    return (
-      <FormBinder
-        callback={(innerValue, updateValue, updateArray) => {
-          const displayValue = innerValue || options.map(() => null);
-
-          return (
-            <Fragment>
-              <FormGroup ref={focus}>
-                {
-                  options.map(
-                    (label, innerIndex) => (
-                      <FormControlLabel
-                        key={label}
-                        {...{ label }}
-                        control={(
-                          <Checkbox
-                            value={label}
-                            checked={!!displayValue[innerIndex]}
-                            color="primary"
-                            onChange={() => updateArray(
-                              innerIndex,
-                              displayValue[innerIndex] ? null : label,
-                              displayValue,
-                            )}
-                          />
-                        )}
-                      />
-                    ),
-                  )
-                }
-              </FormGroup>
-              <SubmitWrap>
-                <GradientButton
-                  filled
-                  text="Save Answer"
-                  clickAction={() => next(index, innerValue)}
-                />
-              </SubmitWrap>
-            </Fragment>
-          );
-        }}
-      />
-    );
-  }
-
-
-  if (type === 'string') {
-    return (
-      <FormBinder
-        callback={(innerValue, updateValue) => (
-          <Fragment>
-            <TextField
-              value={innerValue || ''}
-              variant="outlined"
-              label="Input answer"
-              ref={focus}
-              onChange={event => updateValue(event.target.value)}
-            />
-
-            <SubmitWrap>
-              <GradientButton
-                filled
-                text="Save Answer"
-                clickAction={() => next(index, innerValue)}
-              />
-            </SubmitWrap>
-          </Fragment>
-        )}
-      />
-    );
-  }
-
-  if (type === 'text') {
-    return (
-      <FormBinder
-        callback={(innerValue, updateValue) => (
-          <Fragment>
-            <TextField
-              value={innerValue || ''}
-              multiline
-              rows="4"
-              variant="outlined"
-              label="Input answer"
-              ref={focus}
-              onChange={event => updateValue(event.target.value)}
-            />
-
-            <SubmitWrap>
-              <GradientButton
-                filled
-                text="Save Answer"
-                clickAction={() => next(index, innerValue)}
-              />
-            </SubmitWrap>
-          </Fragment>
-        )}
-      />
-    );
-  }
-
-  if (type === 'select') {
-    return (
-      <GradientSelect
-        value={value}
-        filled
-        options={options.map(text => ({
-          text,
-          callback: () => {
-            next(index, text);
-          },
-        }))}
-      />
-    );
-  }
-
-  if (type === 'boolean') {
-    return (
-      <FlexWrap>
-        <ButtonWrap>
-          <GradientButton
-            text="Yes"
-            clickAction={() => {
-              next(index, 'Yes');
-            }}
-            filled
-          />
-        </ButtonWrap>
-        <ButtonWrap>
-          <GradientButton
-            text="No"
-            clickAction={() => {
-              next(index, 'No');
-            }}
-            filled
-          />
-        </ButtonWrap>
-        <ButtonWrap>
-          <GradientButton
-            text="I don't know"
-            clickAction={() => {
-              next(index, "I don't know");
-            }}
-            filled
-          />
-        </ButtonWrap>
-      </FlexWrap>
-    );
-  }
-
-  if (type === 'gps') {
-    return (
-      <GradientButton
-        text="Calculate Location via GPS"
-        clickAction={() => (
-          navigator.geolocation.getCurrentPosition(
-            ({ coords }) => next(index, `${coords.latitude}, ${coords.longitude}`),
-          )
-        )}
-        filled
-      />
-    );
-  }
-
-  return null;
-};
-
-
-const createItems = (questions, answers, changeAnswer) => {
+const createItems = (questions, answers) => {
   const result = questions.map((questionProp, index) => {
     const summary = answers.length >= index ? answers[index] : null;
 
@@ -248,18 +29,17 @@ const createItems = (questions, answers, changeAnswer) => {
       title: questionProp.title,
       progress: !!summary,
       highlighted: !!summary,
-      content: ({ focus, next }) => (
+      content: item => (
         <Fragment>
           <BodyText>{questionProp.description}</BodyText>
-          {calcInput(
-            questionProp.format,
-            summary,
-            questionProp.options,
-            focus,
-            next,
-            changeAnswer,
-            index,
-          )}
+          <CreateInput
+            {...{ index }}
+            type={questionProp.format}
+            value={summary}
+            options={questionProp.options}
+            next={item.next}
+            focus={item.focusElement}
+          />
         </Fragment>
       ),
     };
@@ -269,27 +49,52 @@ const createItems = (questions, answers, changeAnswer) => {
 };
 
 
-const Markup = ({ questions, answers, changeAnswer, completeAction }) => {
+const Markup = (props) => {
+  const {
+    questions,
+    answers,
+    changeAnswer,
+    onComplete,
+  } = props;
+
   const items = createItems(questions, answers);
   return (
-    <Fragment>
-      <Section>
-        <SectionHeading gutter text="What is this Task?" />
-        <Typography gutterBottom component="p">To get started, tell us a bit about yourself. We are interested in hearing whether you've voted in previous South African national elections and whether you will be voting in the upcoming national election.</Typography>
-        <Typography style={{ marginTop: '20px' }} component="p">Complete this task by filling in the questions below:</Typography>
-      </Section>
-
-      <Section>
-        <SectionHeading gutter text="Start this Task" />
-        <ProgressList
-          {...{ items }}
-          guided={completeAction}
-          updateCallback={changeAnswer}
-        />
-      </Section>
-    </Fragment>
+    <Section>
+      <SectionHeading gutter text="Start this Task" />
+      <ProgressList
+        {...{ items, onComplete }}
+        guided
+        updateCallback={changeAnswer}
+      />
+    </Section>
   );
 };
 
 
 export default Markup;
+
+
+Markup.propTypes = {
+  answers: t.arrayOf(t.string),
+  changeAnswer: t.func.isRequired,
+  onComplete: t.func.isRequired,
+  questions: t.arrayOf(
+    t.shape({
+      title: t.string,
+      description: t.string,
+      format: t.oneOf([
+        'string',
+        'text',
+        'boolean',
+        'select',
+        'checkboxes',
+        'gps',
+      ]),
+    }),
+  ).isRequired,
+};
+
+
+Markup.defaultProps = {
+  answers: [],
+};
