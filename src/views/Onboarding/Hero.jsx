1774aef18af89e75bcfbd4043398f46cbdc33f45
Schalk Venter <venterschalk@gmail.com>
2019-03-01 11:50:46 +0200

    Updates Onboarding view

--- src/views/Onboarding/Hero.jsx
+++ src/views/Onboarding/Hero.jsx
@@ -1,30 +1,14 @@
 import React from 'react';
-import styled from 'styled-components';
+
+
 import illustration from './illustration.svg';
-
-
-const Image = styled.img`
-  display: block;
-  max-height: 343px;
-    max-width: 180px;
-`;
-
-const Wrapper = styled.div`
-  height: 440px;
-  align-items: flex-end;
-  background: #00C77B;
-  flex-grow: 1;
-  width: 100%;
-  padding: 100px 20px 0;
-  justify-content: center;
-  display: flex;
-`;
+import { HeroWrapper, Image } from './styled';
 
 
 const Hero = () => (
-  <Wrapper>
+  <HeroWrapper>
     <Image src={illustration} />
-  </Wrapper>
+  </HeroWrapper>
 );
 
 
