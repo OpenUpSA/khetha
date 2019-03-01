40c46380af682f74e45e74d415869d9b65dc074c
Schalk Venter <venterschalk@gmail.com>
2019-03-01 11:50:58 +0200

    Updates Faq view

---
+++ src/views/Faq/styled.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+
+const FlexWrapper = styled.div`
+  display: flex;
+  width: 100%;
+  align-items: center;
+`;
+
+const FlexQuestion = styled.div`
+  flex-grow: 1;
+`;
+
+
+export {
+  FlexWrapper,
+  FlexQuestion,
+};
+
+
+export default {
+  FlexWrapper,
+  FlexQuestion,
+};
