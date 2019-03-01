12b1abcac32218bbed5f71d7fa08c6b9ee3d6825
Zeeshaan <maudarbocus.zeeshaan@gmail.com>
2019-03-01 14:20:55 +0200

    style faq view

--- src/views/Faq/index.jsx
+++ src/views/Faq/index.jsx
@@ -1,52 +1,106 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import CardActions from '@material-ui/core/CardActions';
 import Button from '@material-ui/core/Button';
 import Layout from '../../components/Layout';
+import SubHeading from '../../components/SectionHeading';
+import GradientButton from '../../components/GradientButton'
 
 
 import { FlexWrapper, FlexQuestion } from './styled';
 
+const CardWrapper = styled(Card)`
+  margin-top: 20px;
+  padding: 10px;
+
+  @media screen and (min-width: 450px) {
+    padding: 20px;
+  }
+`;
+
+const Title = styled(Typography)`
+  padding-bottom: 36px;
+  && {
+    color: #565656;
+    font-size: 20px;
+    font-family: Roboto;
+    font-weight: 500;
+    line-height: normal;
+  }
+`;
+
+const Paragraph = styled(Typography)`
+  padding-bottom: 24px;
+  
+  && {
+    font-family: Roboto;
+    font-weight: normal;
+    line-height: 24px;
+    font-size: 16px;
+    color: rgba(0, 0, 0, 0.541327);
+  }
+`;
+
+const ButtonsGroup = styled.div`
+  width: 100%;
+
+  @media screen and (min-width: 450px) {
+    width: 160px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+`;
+
+const ButtonContainer = styled.div`
+  margin-bottom: 10px;
+
+  @media screen and (min-width: 450px) {
+    margin-bottom: none;
+  }
+`;
 
 const actionsMarkup = (
   <FlexWrapper>
     <FlexQuestion>
       Was this helpful?
     </FlexQuestion>
-    <div>
-      <Button size="small" color="primary">
-        Yes
-      </Button>
-      <Button size="small" color="primary">
-        No
-      </Button>
-    </div>
+    <ButtonsGroup>
+      <ButtonContainer>
+        <GradientButton text="Yes" fullWidth />
+      </ButtonContainer>
+      <ButtonContainer>
+        <GradientButton text="No" fullWidth />
+      </ButtonContainer>
+    </ButtonsGroup>
   </FlexWrapper>
 );
 
 
 const FaqCard = ({ title, body }) => (
-  <Card>
+  <CardWrapper>
     <CardContent>
-      <Typography gutterBottom variant="h5" component="h2">
+      <Title gutterBottom variant="h5" component="h2">
         {title}
-      </Typography>
-      <Typography component="p">
+      </Title>
+      <Paragraph component="p">
         {body}
-      </Typography>
+      </Paragraph>
     </CardContent>
     <CardActions>
       {actionsMarkup}
     </CardActions>
-  </Card>
+  </CardWrapper>
 );
 
 
 const Faq = ({ faqs = [], points }) => (
   <Layout {...{ points }}>
+    <SubHeading text="Frequently Asked Questions" />
     {faqs.map(({ title, body }) => <FaqCard {...{ title, body }} key={title} />)}
   </Layout>
 );
--- src/views/Faq/styled.js
+++ src/views/Faq/styled.js
@@ -2,13 +2,29 @@ import styled from 'styled-components';
 
 
 const FlexWrapper = styled.div`
-  display: flex;
   width: 100%;
+  display: flex;
+  flex-direction: column;
   align-items: center;
+
+  @media screen and (min-width: 450px) {
+    flex-direction: row;
+    align-items: center;
+  }
 `;
 
 const FlexQuestion = styled.div`
   flex-grow: 1;
+  font-family: Roboto;
+  font-weight: 500;
+  line-height: normal;
+  font-size: 14px;
+  color: #01C98B;
+  padding-bottom: 16px;
+
+  @media screen and (min-width: 450px) {
+    padding-bottom: none;
+  }
 `;
 
 
