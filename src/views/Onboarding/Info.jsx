f402cfcd98f7db80a0fbdd6be022d7cb242698ec
Schalk Venter <venterschalk@gmail.com>
2019-03-01 11:50:46 +0200

    Updates Onboarding view

--- src/views/Onboarding/Info.jsx
+++ src/views/Onboarding/Info.jsx
@@ -1,42 +1,15 @@
 import React from 'react';
-import styled from 'styled-components';
-import { Typography } from '@material-ui/core';
 
 
-const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: 100%;
-  max-width: 300px;
-  text-align: center;
-  padding: 40px 20px 15px;
-`;
-
-
-const Heading = styled(Typography)`
-  && {
-    font-weight: bold;
-    font-size: 26px;
-    line-height: 32px;
-  }
-`;
-
-const Description = styled(Typography)`
-  && {
-    font-size: 16px;
-    color: #8C8C8C;
-    margin-bottom: 35px;
-  }
-`;
+import { InfoWrapper, Heading, InfoDescription } from './styled';
 
 
 const Info = ({ title, description, children }) => (
-  <Wrapper>
+  <InfoWrapper>
     <Heading gutterBottom component="h1">{title}</Heading>
-    <Description component="p">{description}</Description>
+    <InfoDescription component="p">{description}</InfoDescription>
     {children}
-  </Wrapper>
+  </InfoWrapper>
 );
 
 
