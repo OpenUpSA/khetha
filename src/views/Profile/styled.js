52ca76f2d1a5c6fdeb4da35dfd86d0a5e483ef54
Schalk Venter <venterschalk@gmail.com>
2019-03-01 11:51:11 +0200

    Updates Profile view

--- src/views/Profile/styled.js
+++ src/views/Profile/styled.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import TableCell from '@material-ui/core/TableCell';
 
 
 const Section = styled.div`
@@ -6,11 +7,38 @@ const Section = styled.div`
 `;
 
 
+const OptionalCell = styled(TableCell)`
+  && {
+    display: none;
+
+    @media screen and (min-width: 750px) {
+      display: table-cell;
+    }
+  }
+`;
+
+const NameWrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+
+const NameText = styled.div`
+  padding-left: 10px;
+`;
+
+
 export {
   Section,
+  OptionalCell,
+  NameWrapper,
+  NameText,
 };
 
 
 export default {
   Section,
+  OptionalCell,
+  NameWrapper,
+  NameText,
 };
