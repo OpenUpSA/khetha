d9dbd583f0b44b5df526b4179ae66ab7c8d86086
Schalk Venter <venterschalk@gmail.com>
2019-03-03 23:44:48 +0200

    Updates Gatsby pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,48 +1,43 @@
-import { createElement } from 'react';
+import { connect } from 'react-redux';
+import { createElement, Component } from 'react';
 import { navigate } from 'gatsby';
-import OnBoarding from '../views/Onboarding';
 
 
-const props = {
-  complete: () => {
-    setTimeout(
-      () => navigate('/start'),
-      1000,
-    );
-  },
-  translation: {
-    intro: {
-      title: 'What is Khetha?',
-      description:
-        'Kheta is an online game where you can win prizes just by sharing your thoughts on the Mzansi 2019 election.',
-      primary: 'Select a language',
-      secondary: 'Start',
-    },
-    prizes: {
-      title: 'Want to win?',
-      description: 'Complete tasks to unlock levels and stand a chance to win the following:',
-      primary: 'Continue',
-      list: [
-        {
-          title: 'Free Airtime',
-          description:
-            'You will receive a guaranteed rewards of free airtime by completing your first task!',
-        },
-        {
-          title: 'R25 000 worth of prizes',
-          description:
-            'You will become eligible for more weekly/monthly prizes as you unlock more levels.',
-        },
-      ],
-    },
-    alert: {
-      title: 'Very Important!',
-      description:
-        'In order for us to arrange the delivery of your first reward and additional rewards you need to allow notifications:',
-      primary: 'I understand',
-    },
-  },
-};
+import Loading from '../views/Loading';
 
 
-export default () => createElement(OnBoarding, props);
+const stateToProps = (state, ownProps) => ({
+  allAnswers: state.answers,
+  ...ownProps,
+});
+
+
+const dispatchToProps = (dispatch, ownProps) => ({
+  ...ownProps,
+});
+
+
+const connectToReduxStore = connect(stateToProps, dispatchToProps);
+
+
+class Page extends Component {
+  componentDidMount() {
+    const { allAnswers } = this.props;
+
+    // if (allAnswers) {
+    //   return navigate('/progress/');
+    // }
+
+    return navigate('/start/');
+  }
+
+  render() {
+    return createElement(Loading);
+  }
+}
+
+
+const PageWithRedux = connectToReduxStore(Page);
+
+
+export default PageWithRedux;
--- src/pages/start/index.js
+++ src/pages/start/index.js
@@ -1,66 +1,162 @@
-import { createElement } from 'react';
-import { navigate } from 'gatsby';
+import { connect } from 'react-redux';
+import { createElement, Component } from 'react';
+import { navigate, graphql } from 'gatsby';
+import { difference } from 'lodash';
+
+
+import { create } from '../../redux/modules/answers';
+import Loading from '../../views/Loading';
+import { requestNotificationAccess } from '../../redux/actions';
 import Start from '../../views/Start';
-import Layout from '../../components/Layout';
 
 
-const rewards = [
-  {
-    id: 'airtime',
-    points: 10,
-    title: 'R50 Airtime Voucher',
-    description:
-      'As a thank you for the time taken to complete a couple of initial tasks we will be awarding several participants with R50 airtime at the end of Febraury. Learn more about our other exciting prizes in your profile section.',
-    dates: ['28 February 2019'],
-  },
-];
+export const query = graphql`query {
+  tasks: allTasksJson {
+    edges {
+      node {
+        id
+        icon
+        points
+        eng {
+          title
+          description
+          questions {
+            title
+          }
+        }
+      }
+    }
+  }
+  rewards: allRewardsJson {
+    edges {
+      node {
+        id
+        title
+        description
+        dates
+        points
+      }
+    }
+  }
+}`;
 
 
-const translation = {
-  view: {
-    points: 'Kheta Points',
-    filter: {
-      title: 'Filter tasks',
-      active: 'Filtered by',
-      difficulty: [
-        'Show all',
-        'Easy',
-        'Medium',
-        'Hard',
-        'Very hard',
-      ],
+const buildRewards = rewards => rewards.edges.map(({ node }) => ({
+  id: node.id,
+  points: node.points,
+  title: node.title,
+  description: node.description,
+  dates: node.dates,
+}));
+
+
+const buildTasks = tasks => tasks.edges.map(({ node }) => ({
+  id: node.id,
+  points: node.points,
+  title: node.eng.title,
+  description: node.eng.description,
+  icon: node.icon,
+  amountOfQuestions: node.eng.questions.length,
+}));
+
+
+const stateToProps = (state, ownProps) => ({
+  points: state.info.points,
+  activeTasks: state.answers ?
+    Object.keys(state.answers).filter(key => !!state.answers[key].completed)
+    : [],
+  // activeTasks: state.answers ? Object.keys(state.answers) : [],
+  ...ownProps,
+});
+
+
+const dispatchToProps = (dispatch, ownProps) => ({
+  startTask: (id, amount) => dispatch(create(id, amount)),
+  onMount: () => dispatch(requestNotificationAccess()),
+  ...ownProps,
+});
+
+
+const connectToReduxStore = connect(stateToProps, dispatchToProps);
+
+
+const createProps = (props) => {
+  const {
+    tasks,
+    rewards,
+    points,
+    onMount,
+    activeTasks,
+    startTask,
+  } = props;
+
+  const validTasks = difference(tasks.map(({ id }) => id), activeTasks);
+
+  return {
+    rewards,
+    tasks: validTasks.map(id => tasks.find(({ id: taskId }) => id === taskId)),
+    points,
+    onMenuButtonPress: navigate,
+    onMount,
+    onCardPress: ({ amountOfQuestions, id }) => {
+      startTask(id, amountOfQuestions);
+      return navigate(`/task/index.html?id=${id}`);
     },
-    more: {
-      title: 'Need more tasks?',
-      description: 'We are continually adding new tasks to this app as the build up to the 2019 election continues. Learn more about our other exciting prizes in your profile section.',
+    translation: {
+      points: 'Khetha Points',
+      filter: {
+        title: 'Filter tasks',
+        active: 'Filtered by',
+        difficulty: ['Show all', 'Easy', 'Medium', 'Hard', 'Very Hard'],
+      },
+      more: {
+        title: 'Need more tasks?',
+        description: 'We are continually adding new tasks to this app as the build up to the 2019 election continues.',
+      },
+      rewards: {
+        qualify: ({ nextDraw }) => `You qualify for the next draw in ${nextDraw}!`,
+        noQualify: ({ nextDraw, remaining }) => `${remaining} more points needed to qualify for the upcoming draw in ${nextDraw}`,
+      },
     },
-  },
-  rewards: {
-    qualify: ({ nextDraw }) => `You qualify for the next draw in ${nextDraw}!`,
-    noQualify: ({ nextDraw, remaining }) => `${remaining} more points needed to qualify for the upcoming draw in ${nextDraw}`,
-  },
+  };
 };
 
 
-const props = {
-  rewards,
-  translation,
-  clickAction: () => navigate('/task'),
-  points: 0,
-  tasks: [
-    {
-      id: 'personal-info',
-      points: 1,
-      title: 'Tell us about yourself',
-      description: "To get started, tell us a bit about yourself. We are interested in hearing whether you've voted in previous South African national elections and whether you will be voting in the upcoming national election.",
-      icon: 'Person',
-    },
-  ],
-};
+class Page extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: true,
+    };
+  }
+
+  componentDidMount() {
+    return this.setState({ loading: false });
+  }
+
+  render() {
+    const { props, state } = this;
+
+    if (state.loading) {
+      return createElement(Loading);
+    }
+
+    const passedProps = createProps({
+      startTask: props.startTask,
+      activeTasks: props.activeTasks,
+      points: props.points,
+      rewards: buildRewards(props.data.rewards),
+      tasks: buildTasks(props.data.tasks),
+      onMount: props.onMount,
+    });
+
+    return createElement(Start, passedProps);
+  }
+}
 
 
-export default () => createElement(
-  Layout,
-  { points: 0 },
-  createElement(Start, props),
-);
+const connectedPage = connectToReduxStore(Page);
+
+
+export default connectedPage;
