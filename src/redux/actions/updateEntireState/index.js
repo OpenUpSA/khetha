4e5bc01bc5a6bffd71b3074af2efd4a5d18c0682
Schalk Venter <venterschalk@gmail.com>
2019-03-03 23:56:43 +0200

    Fixes linting issues

--- src/redux/actions/updateEntireState/index.js
+++ src/redux/actions/updateEntireState/index.js
@@ -1,11 +1,11 @@
-import { replaceEntireState as replaceUser } from '../../modules/info';
-import { replaceEntireState as replaceStorage } from '../../modules/storage';
-import { replaceEntireState as replaceTasks } from '../../modules/tasks';
+// // import { replaceEntireState as replaceUser } from '../../modules/info';
+// // import { replaceEntireState as replaceStorage } from '../../modules/storage';
+// // import { replaceEntireState as replaceTasks } from '../../modules/tasks';
 
 
-export default ({ storage, info, tasks }) => (dispatch) => {
-  dispatch(replaceStorage({ state: storage }));
-  dispatch(replaceUser({ state: info }));
-  dispatch(replaceTasks({ state: tasks }));
-  return { storage, info, tasks };
-};
+// export default ({ storage, info, tasks }) => (dispatch) => {
+//   // dispatch(replaceStorage({ state: storage }));
+//   // dispatch(replaceUser({ state: info }));
+//   // dispatch(replaceTasks({ state: tasks }));
+//   // return { storage, info, tasks };
+// };
