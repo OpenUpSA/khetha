878bcffdb277ef4afd336eaec7df7bb28c7f1935
Schalk Venter <venterschalk@gmail.com>
2019-03-03 23:42:27 +0200

    Updates Redux logic

--- src/redux/modules/info/index.js
+++ src/redux/modules/info/index.js
@@ -1,119 +1,67 @@
-import { auth } from '../../../helpers/firebaseHelpers';
-import createDispatchPipe from '../../../helpers/createDispatchPipe';
-
 const REPLACE_STATE = 'info/REPLACE_STATE';
 const SET_ID = 'info/SET_ID';
-const REPLACE_VALUE = 'info/REPLACE_VALUE';
-const REPLACE_VALUE_AND_TIMESTAMP = 'info/REPLACE_VALUE_AND_TIMESTAMP';
-const LOG_NEW_GUEST_ACCOUNT = 'info/LOG_NEW_GUEST_ACCOUNT';
-const CHANGE_LANGUAGE = 'info/CHANGE_LANGUAGE';
 const ADD_NOTIFICATION_TOKEN = 'info/ADD_NOTIFICATION_TOKEN';
+const ADD_POINTS = 'info/ADD_POINTS';
 
-
-export default (state = {}, action = {}) => {
+const reducer = (state = {}, action = {}) => {
   switch (action.type) {
     case REPLACE_STATE: return action.payload.state;
 
+
     case SET_ID: return {
       ...state,
-      id: action.payload,
+      id: action.payload.id,
+      joined: action.payload.timestamp,
     };
 
-    case CHANGE_LANGUAGE: return {
-      ...state,
-      language: {
-        timestamp: action.payload.time,
-        value: action.payload.isoKey,
-      },
-    };
 
     case ADD_NOTIFICATION_TOKEN: return {
       ...state,
-      notifications: [
-        ...state.notifications,
-        {
-          timestamp: action.payload.time,
-          value: action.payload.token,
-        },
-      ],
+      lastAction: action.payload.timestamp,
+      notification: action.payload.token,
     };
 
-    case REPLACE_VALUE: return {
+
+    case ADD_POINTS: return {
       ...state,
-      [action.payload.key]: action.payload.value,
+      points: state.points + action.payload,
     };
 
-    case REPLACE_VALUE_AND_TIMESTAMP: return {
-      ...state,
-      [action.payload.key]: {
-        timestamp: action.payload.time,
-        value: action.payload.value,
-      },
-    };
 
     default: return state;
   }
 };
 
 
-export const replaceEntireState = ({ state }) => ({
-  type: REPLACE_STATE,
+const addUser = id => ({
+  type: SET_ID,
   payload: {
-    state,
+    id,
+    timestamp: new Date().getTime(),
   },
 });
 
 
-export const setUserValue = ({ key, value, timestamp }) => {
-  if (!timestamp) {
-    return {
-      type: REPLACE_VALUE,
-      payload: {
-        key,
-        value,
-      },
-    };
-  }
-
-  return {
-    type: REPLACE_VALUE_AND_TIMESTAMP,
-    payload: {
-      time: new Date().getTime(),
-      key,
-      value,
-    },
-  };
-};
-
-
-export const addNotificationToken = ({ token }) => ({
+const addNotificationToken = token => ({
   type: ADD_NOTIFICATION_TOKEN,
   payload: {
-    time: new Date().getTime(),
     token,
+    timestamp: new Date().getTime(),
   },
 });
 
 
-export const changeLanguage = ({ isoKey }) => ({
-  type: REPLACE_VALUE_AND_TIMESTAMP,
-  payload: {
-    time: new Date().getTime(),
-    isoKey,
-  },
+const addPoints = points => ({
+  type: ADD_POINTS,
+  payload: points,
 });
 
-export const createNewGuestAccount = () => (dispatch) => {
-  const dispatchPipe = createDispatchPipe(dispatch);
-  const logNewGuestAccount = dispatchPipe({ type: LOG_NEW_GUEST_ACCOUNT });
 
-  const promise = new Promise((resolve) => {
-    const result = auth
-      .signInAnonymously()
-      .then(logNewGuestAccount)
-      .then(resolve);
-    return result;
-  });
-
-  return promise;
+export {
+  addUser,
+  addNotificationToken,
+  addPoints,
 };
+
+
+export default reducer;
