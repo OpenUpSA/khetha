43df352816fb7301235966a119e31148af56bfa0
Schalk Venter <venterschalk@gmail.com>
2019-03-04 11:55:46 +0200

    Updates pages

--- src/pages/task/index.js
+++ src/pages/task/index.js
@@ -15,6 +15,7 @@ export const query = graphql`query {
     edges {
       node {
         id
+        points
         eng {
           title
           questions {
@@ -36,6 +37,7 @@ const addId = (values, index) => ({ ...values, id: index });
 const buildTasks = ({ edges }) => edges.map(({ node }) => ({
   id: node.id,
   title: node.eng.title,
+  points: node.points,
   questions: node.eng.questions.map(addId),
 }));
 
@@ -84,9 +86,7 @@ const createProps = (props, id) => {
   const {
     data,
     allAnswers,
-    // allTasks,
     points,
-    // title,
     changeAnswer,
     submit,
   } = props;
@@ -105,13 +105,13 @@ const createProps = (props, id) => {
     points,
     onMenuButtonPress: navigate,
     questions: task.questions,
-    answers: answers && answers.map(item => (item ? item.value : null)),
+    answers: !!answers && answers.map(item => (item ? item.value : null)),
     onQuestionSave: ({ index, value }) => changeAnswer(
       id,
       formatForSave({ answers, index, value }),
     ),
     onTaskSubmit: () => {
-      submit(id, points);
+      submit(id, task.points);
       return navigate('/start/');
     },
   };
