8e49e5d8e235d750ccf34c5bc8691020b6c9c235
Zeeshaan <maudarbocus.zeeshaan@gmail.com>
2019-03-04 13:43:12 +0200

    redoing progress cards

--- src/views/Progress/index.jsx
+++ src/views/Progress/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 
 import Layout from '../../components/Layout';
+import ProgressCards from '../../components/ProgressCards';
 
 
 const Progress = (props) => {
@@ -9,17 +10,27 @@ const Progress = (props) => {
     onMenuButtonPress,
     points,
     onCardPress,
+    tasks,
   } = props;
 
 
   return (
-    <Layout {...{ points, onMenuButtonPress}}>
-      <pre>
-        <code style={{ display: 'block', background: 'black', color: 'white', whiteSpace: 'pre-wrap' }}>
-          {JSON.stringify(props, 0, 2)}
-        </code>
-        <button onClick={() => onCardPress(3)}>example card click - should return id (example 3)</button>
-      </pre>
+    <Layout {...{ points, tasks}}>
+      <button onClick={() => onCardPress(3)}>example card click - should return id (example 3)</button>
+      {tasks.map(({
+        id,
+        title,
+        icon,
+        progress,
+      }) => (
+        <ProgressCards
+          key={id}
+          title={title}
+          icon={icon}
+          progress={progress}
+          onCardPress={() => onCardPress(3)}
+        />
+      ))}
     </Layout>
   );
 }
