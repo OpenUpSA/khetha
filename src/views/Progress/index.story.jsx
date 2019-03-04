52bd21fcfeec8e855f4d13ef4e95ee602a981dd0
Schalk Venter <venterschalk@gmail.com>
2019-03-04 08:35:37 +0200

    Adds Progress view

---
+++ src/views/Progress/index.story.jsx
@@ -0,0 +1,40 @@
+/* eslint-disable import/no-extraneous-dependencies */
+
+
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import faker from 'faker';
+
+
+import { names } from '../../config/icons';
+import { randomLengthBlankArray, randomNumber } from '../../helpers/randomizer';
+import Progress from './index';
+
+
+const namesLength = names.length < 1 ? 1 : names.length - 1;
+
+
+const createProps = (seed) => {
+  if (seed) {
+    faker.seed(seed);
+  }
+
+  return {
+    points: randomNumber(0, 110),
+    onMenuButtonPress: console.log,
+    onCardPress: console.log,
+    tasks: randomLengthBlankArray(1, 30).map((value, index) => ({
+      id: index,
+      title: faker.commerce.productName(),
+      icon: names[randomNumber(0, namesLength)],
+      progress: randomNumber(0, 100),
+    })),
+  };
+};
+
+
+const basic = () => <Progress {...createProps()} />;
+
+
+storiesOf('view.Progress', module)
+  .add('Basic', basic);
