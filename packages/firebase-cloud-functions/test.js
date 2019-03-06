df08433368229aac8e0801a6c89baee5ad8bba37
Schalk Venter <venterschalk@gmail.com>
2019-03-06 08:11:40 +0200

    Fixes issue with API test

--- packages/firebase-cloud-functions/test.js
+++ packages/firebase-cloud-functions/test.js
@@ -5,6 +5,8 @@ const axios = require('axios');
 
 
 const url = 'http://localhost:5000/khetha-project/us-central1/sync';
-return axios.post(url, { id: 'test-user', newState: { test: true } })
+
+
+axios.post(url, { id: 'test-user', newState: { test: true } })
   .then(({ data }) => console.log(data))
   .catch(console.warn);
