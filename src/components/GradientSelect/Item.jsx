c0075c2f20131ae23177936273159ee357bf1a90
Zeeshaan <maudarbocus.zeeshaan@gmail.com>
2019-03-06 15:32:48 +0200

    Add propTypes to GradientSelect

--- src/components/GradientSelect/Item.jsx
+++ src/components/GradientSelect/Item.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import t from 'prop-types';
 
 
 import { StyledItem } from './styled';
@@ -27,3 +28,20 @@ const Item = (props) => {
 
 
 export default Item;
+
+Item.propTypes = {
+  /** The text that goes in the dropdown selection */
+  text: t.string.isRequired,
+  /** An index number associated with each drop down selection */
+  id: t.number.isRequired,
+  /** Checks whether it is specifically the reset button */
+  reset: t.bool,
+  /** Disables the option in the drop down */
+  disabled: t.bool,
+};
+
+
+Item.defaultProps = {
+  reset: false,
+  disabled: false,
+};
