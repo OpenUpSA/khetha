c570b6655cf5d8eb5eb8f2f81ee207fae9c5257d
Adi Eyal <adi@openup.org.za>
2019-03-09 18:01:33 +0200

    Added render() to /bigdebate

--- src/pages/bigdebate/index.js
+++ src/pages/bigdebate/index.js
@@ -1,8 +1,21 @@
-import { Component } from 'react';
-import { navigate } from 'gatsby';
+import { createElement, Component } from 'react';
+import { navigate, } from 'gatsby';
+import Loading from '../../views/Loading';
+
+
+import Task from '../../views/Task';
+
 
 class Page extends Component {
+
   componentDidMount() {
-    return navigate('/bigdebatepoll');
+      return navigate('/bigdebatepoll');
+  }
+
+  render() {
+    return createElement(Loading, {});
   }
 }
+
+export default Page;
+
