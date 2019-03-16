aa6c7a0e7c173390c0338432150a4b6c07b28182
Schalk Venter <venterschalk@gmail.com>
2019-03-16 08:00:26 +0200

    Moves integration keys into .ENV file on Netlify

--- src/tooling/gatsby-config/createAnalyticsConfig.js
+++ src/tooling/gatsby-config/createAnalyticsConfig.js
@@ -1,8 +1,8 @@
-module.exports = () => [
+module.exports = trackingId => [
   {
     resolve: 'gatsby-plugin-google-analytics',
     options: {
-      trackingId: 'UA-44046318-5',
+      trackingId,
     },
   },
 ];
