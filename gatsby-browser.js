4192bf0daf2de7c34f7f8aaeaa048e5e00f6000a
Schalk Venter <venterschalk@gmail.com>
2019-03-19 07:56:58 +0200

    Adds sentry initialisation

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,20 +1,20 @@
 /* eslint-disable import/prefer-default-export */
-
+/* eslint-disable import/no-extraneous-dependencies */
 
 import { createElement } from 'react';
 import { Provider } from 'react-redux';
-
+import * as Sentry from '@sentry/browser';
 
 import { createUser } from './src/redux/actions';
 import store from './src/redux/store';
-
 
 const wrapRootElement = ({ element }) => (
   createElement(Provider, { store }, element)
 );
 
-
 const onClientEntry = () => {
+  Sentry.init({ dsn: 'https://4beb31b94ed742d9aeeb981a354fc690@sentry.io/1416880' });
+
   const { info } = store.getState();
   const { onboarded, id } = info;
 
