cc7538089e9cc964996463f023dbf59f99651a09
Schalk Venter <venterschalk@gmail.com>
2019-03-19 11:58:57 +0200

    Removes hardcoded error

--- src/views/Onboarding/index.jsx
+++ src/views/Onboarding/index.jsx
@@ -14,10 +14,6 @@ export default class Onboarding extends Component {
     };
   }
 
-  componentDidMount() {
-    console.log(notExist.example);
-  }
-
   changeLanguage = (language) => {
     const { nextStep } = this;
     this.setState({ language });
