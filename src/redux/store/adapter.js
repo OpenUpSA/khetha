ec2568a5aa3e40bae8855226ab819197b5a237e2
Shailen <shailen@gettruck.co.za>
2019-04-02 16:25:58 +0200

    Modified createOldAnswersToStartedTasks to return objects instead of array

--- src/redux/store/adapter.js
+++ src/redux/store/adapter.js
@@ -4,39 +4,44 @@ const checkIfPropExists = prop => localStorage.getItem(prop) !== null;
 const getProp = prop => JSON.parse(localStorage.getItem(prop));
 const createProp = data => localStorage.setItem('30_03_2019', JSON.stringify(data));
 
-const convertOldInfoToNewInfo = ({ info, storage: { synced } }) => Object.assign(info, { synced });
+const convertOldInfoToNewInfo = ({ info, storage: { synced } }) => ({
+  ...info,
+  synced,
+});
 
-const convertOldAnswersToStartedTasks = ({ answers }) => {
-  const keys = Object.keys(answers);
+const createAnswerObject = key => ({ value, answered, ...otherValues }, index) => {
 
-  return keys.map((key) => {
-    const { completed, started, data } = answers[key];
-    const [{ eng: { questions } }] = tasks.filter(({ id }) => id === key);
+  const [{ eng: { questions } }] = tasks.filter(({ id }) => id === key);
 
-    // eslint-disable-next-line object-curly-newline
-    const newAnswers = data.map(({ answered, edits, lastEdit, value }, index) => ({
-      question: questions[index].title,
-      answer: value,
-      firstAnswered: answered,
-      edits,
+  return {
+    ...otherValues,
+    answer: value,
+    firstAnswered: answered,
+    question: questions[index].title,
+  };
+};
+
+const convertOldAnswersToStartedTasks = oldTasks => (result, key) => {
+  const { completed: lastEdit, started, data } = oldTasks[key];
+
+  return {
+    ...result,
+    [key]: {
       lastEdit,
-    }));
-
-    return {
-      id: key,
-      lastEdit: completed,
       started,
-      answers: newAnswers,
-    };
-  });
+      answers: data.map(createAnswerObject(key)),
+    },
+  };
 };
 
 const convertOldSchemaToNewSchema = () => {
   const oldStore = getProp('06_03_2019');
 
+  const keys = Object.keys(oldStore.answers);
+
   const newStore = {
     info: convertOldInfoToNewInfo(oldStore),
-    startedTasks: convertOldAnswersToStartedTasks(oldStore),
+    startedTasks: keys.reduce(convertOldAnswersToStartedTasks(oldStore.answers), {}),
   };
 
   createProp(newStore);
