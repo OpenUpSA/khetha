6537d26c138fb7fb85db711ba4fc6ef4d6529084
Carlos Gonzalez <homearanya@gmail.com>
2019-04-03 10:49:57 +0200

    add faqs and rewards collections to Neflify CMS

---
+++ src/cms/collections/createRewards.js
@@ -0,0 +1,50 @@
+const rewardsRecord = () => [
+  // {
+  //   name: "id",
+  //   label: "Id",
+  //   widget: "string"
+  // },
+  {
+    name: "points",
+    label: "Points",
+    widget: "number",
+    valueType: "int"
+  },
+  {
+    name: "title",
+    label: "Title",
+    widget: "string"
+  },
+  {
+    name: "description",
+    label: "Description",
+    widget: "markdown"
+  },
+  {
+    name: "dates",
+    label: "Dates",
+    widget: "list",
+    hint: "List of dates. Separated by commas. Format: YYYY-MM-DD"
+  }
+];
+
+const createRewards = () => ({
+  name: "rewards",
+  identifier_field: "id",
+  label: "Rewards",
+  label_singular: "Reward",
+  folder: "src/data/rewards/",
+  extension: "json",
+  create: true,
+  fields: [
+    {
+      name: "type",
+      label: "Type",
+      widget: "hidden",
+      default: "rewards"
+    },
+    ...rewardsRecord()
+  ]
+});
+
+export default createRewards;
--- src/cms/index.js
+++ src/cms/index.js
@@ -1,29 +1,28 @@
 /* eslint-disable import/no-extraneous-dependencies */
 
-import CMS, { init } from 'netlify-cms';
-import IconSelector from './widgets/IconSelector';
-import IconSelectorMarkup from './widgets/IconSelector/Markup';
-import createTasks from './collections/createTasks';
+import CMS, { init } from "netlify-cms";
+import IconSelector from "./widgets/IconSelector";
+import IconSelectorMarkup from "./widgets/IconSelector/Markup";
+import createTasks from "./collections/createTasks";
+import createFaqs from "./collections/createFaqs";
+import createRewards from "./collections/createRewards";
 
 const config = {
   backend: {
-    name: 'github',
-    repo: 'OpenUpSA/khetha',
+    name: "github",
+    repo: "OpenUpSA/khetha"
   },
-  media_folder: 'static/assets',
-  public_folder: 'assets',
-  publish_mode: 'editorial_workflow',
+  media_folder: "static/assets",
+  public_folder: "assets",
+  publish_mode: "editorial_workflow",
   load_config_file: false,
   editor: {
-    preview: false,
+    preview: false
   },
-  collections: [
-    createTasks(),
-  ],
+  collections: [createTasks(), createFaqs(), createRewards()]
 };
 
+CMS.registerWidget("icon-selector", IconSelector, IconSelectorMarkup);
 
-CMS.registerWidget('icon-selector', IconSelector, IconSelectorMarkup);
-
-
+console.log("config", config);
 init({ config });
